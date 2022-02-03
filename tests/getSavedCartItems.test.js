a31373b97bb32955c0f713262ba0ed29f6fe8224
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-02-03 18:06:26 -0300

    Requisito 10 - realizado

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,10 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+it('Ao executar getSavedCartItems, o método localStorage.getItem deve ser chamado', async () => {
+await getSavedCartItems()
+});
+  it('Ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro.', async () => {
+
+  });
 });
